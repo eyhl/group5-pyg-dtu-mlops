ec77da36e1faffb31df6c5b59c2a6ff138bfbf2f
lenhy <lenhy@dtu.dk>
2022-01-07 14:40:04 +0100

    Typing

--- src/models/model.py
+++ src/models/model.py
@@ -1,16 +1,17 @@
 from torch_geometric.nn import GCNConv
 import torch.nn.functional as F
 from torch import nn
+import torch
 
 
 class GCN(nn.Module):
-    def __init__(self, hidden_channels, num_features, num_classes, dropout):
+    def __init__(self, hidden_channels: int, num_features: int, num_classes: int, dropout: float) -> None:
         super().__init__()
         self.conv1 = GCNConv(num_features, hidden_channels)
         self.conv2 = GCNConv(hidden_channels, num_classes)
         self.dropout = nn.Dropout(dropout)
 
-    def forward(self, x, edge_index):
+    def forward(self, x: torch.Tensor, edge_index: torch.Tensor) -> torch.Tensor:
         x = self.conv1(x, edge_index)
         x = self.dropout(F.relu(x))
         x = self.conv2(x, edge_index)
