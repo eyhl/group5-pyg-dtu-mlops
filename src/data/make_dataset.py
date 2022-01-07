e287ac48b2aa8e660d12cdb279c59b04a63608e7
lenhy <lenhy@dtu.dk>
2022-01-07 13:35:22 +0100

    Bugfix

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -2,7 +2,7 @@
 from torch_geometric.datasets import Planetoid
 from torch_geometric.transforms import NormalizeFeatures
 
-s
+
 def load_data(path, name):
     dataset = Planetoid(root=path, name=name, transform=NormalizeFeatures())
     return dataset[0]
