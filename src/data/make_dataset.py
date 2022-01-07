4e3298ef7a026193efd9a0f99acd167e7dcdd5b2
lenhy <lenhy@dtu.dk>
2022-01-07 14:22:44 +0100

    typing and docstrings

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,8 +1,18 @@
 # -*- coding: utf-8 -*-
+import torch_geometric.data
 from torch_geometric.datasets import Planetoid
 from torch_geometric.transforms import NormalizeFeatures
 
 
-def load_data(path, name):
+def load_data(path: str, name: str) -> torch_geometric.data.Data:
+    """
+    Loads the data from {path}/{name} or downloads it and saves it into the directory.
+    :param path: Path to the dataset
+    :param name: Name of the dataset
+    :return: data -- the inputs are stored in data.x, labels in data.y,
+                     train and test masks in data.train_mask and data.test_mask
+    """
+    # Get torch_geometric.data.InMemoryDataset dataset
     dataset = Planetoid(root=path, name=name, transform=NormalizeFeatures())
+    # Return only the data
     return dataset[0]
