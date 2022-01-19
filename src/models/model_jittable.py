663e6da891b1803820a680130dbf2c77b7151f88
KasiaOtko <katin010@gmail.com>
2022-01-19 11:48:12 +0100

    Preparing for local deployment

---
+++ src/models/model_jittable.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn.functional as F
+from torch import nn
+from torch_geometric.nn import GCNConv  # type: ignore
+
+
+class GCN(nn.Module):
+    def __init__(
+        self, hidden_channels: int, num_features: int, num_classes: int, dropout: float
+    ) -> None:
+        super().__init__()
+        self.conv1 = GCNConv(num_features, hidden_channels).jittable()
+        self.conv2 = GCNConv(hidden_channels, num_classes).jittable()
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x: torch.Tensor, edge_index: torch.Tensor) -> torch.Tensor:
+        if x.ndim != 2:
+            raise ValueError("Expected input is not a 2D tensor,"
+                             f"instead it is a {x.ndim}D tensor.")
+        if x.shape[1] != 1433:
+            raise ValueError("Feature vector should be of size 1433.")
+        x = self.conv1(x, edge_index)
+        x = self.dropout(F.relu(x))
+        x = self.conv2(x, edge_index)
+        return x
