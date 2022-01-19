3c4d163247b2f3182574b65626684b7528f64e3b
KasiaOtko <katin010@gmail.com>
2022-01-19 14:59:05 +0100

    Custom handler update

--- src/models/graph_handler.py
+++ src/models/graph_handler.py
@@ -1,4 +1,42 @@
 import torch
-from torchserve.torch_handler.base_handler import BaseHandler
+from ts.torch_handler.base_handler import BaseHandler
 
-class CustomeGraphHandler(BaseHandler):
+class CustomGraphHandler(BaseHandler):
+    
+    def preprocess(self, data):
+        """
+        Preprocess function to convert the request input to a tensor(Torchserve supported format).
+        The user needs to override to customize the pre-processing
+        Args :
+            data (list): List of the data from the request input.
+        Returns:
+            tensor: Returns the tensor data of the input
+        """
+        x = data.x.clone()
+        edge_index = data.edge_index.clone()
+        y = data.y.clone()
+        return torch.as_tensor(x, device=self.device), torch.as_tensor(edge_index, device=self.device), torch.as_tensor(y, device=self.device)
+
+
+    def inference(self, model_input):
+        """
+        Internal inference methods
+        :param model_input: transformed model input data
+        :return: list of inference output in NDArray
+        """
+        # Do some inference call to engine here and return output
+        model_output = self.model.forward(model_input)
+        return model_output
+
+
+    def handle(self, data, context):
+        """
+        Invoke by TorchServe for prediction request.
+        Do pre-processing of data, prediction using model and postprocessing of prediciton output
+        :param data: Input data for prediction
+        :param context: Initial context contains model server system properties.
+        :return: prediction output
+        """
+        model_input = self.preprocess(data)
+        model_output = self.inference(model_input)
+        return self.postprocess(model_output)
